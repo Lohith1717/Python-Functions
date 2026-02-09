168cee995723ed9e8e56404432a040d7dadfc2cf
Lohith Kumar B N <lohithkumar88617@gmail.com>
2026-02-09 11:49:07 +0530

    Add files via upload

---
+++ Python Functions.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    result = a + b
+    print("Sum =", result)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+add_numbers(num1, num2)
